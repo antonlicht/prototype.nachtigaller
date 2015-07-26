798222eaf58717be82aa75e6f16d3fd2d19a8da3
Anton Licht <anton.licht@gmail.com>
2015-07-26 14:22:09 +0200

    Implement CameraPath

---
+++ Assets/Editor/CameraPathInspector.cs
@@ -0,0 +1,116 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraPath))]
+public class CameraPathInspector : Editor
+{
+    private const float HandleSize = 0.04f;
+    private const float PickSize = 0.06f;
+
+    private int _selectedIndex = -1;
+    private CameraPath _spline;
+    private Transform _handleTransform;
+    private Quaternion _handleRotation;
+
+    void OnEnable()
+    {
+        Tools.hidden = true;
+    }
+
+    void OnDisable()
+    {
+        Tools.hidden = false;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        _spline = target as CameraPath;
+        _selectedIndex = _spline.GetSelectedPoint(_spline.Time);
+        if (_selectedIndex >= 0 && _spline.IsPathPoint(_selectedIndex))
+        {
+            _spline.SetPoint(_selectedIndex, _spline.transform.position);
+            _spline.SetRotation(_selectedIndex, _spline.transform.rotation);
+        }
+        EditorGUI.BeginChangeCheck();
+        _spline.Time = EditorGUILayout.Slider("Time", _spline.Time, 0f, 1f);
+        if (EditorGUI.EndChangeCheck())
+        {
+            var selection = _spline.GetSelectedPoint(_spline.Time);
+            _selectedIndex = selection;
+        }
+
+        if (GUILayout.Button("Add Point"))
+        {
+            Undo.RecordObjects(new Object[] { _spline.transform, _spline }, "Add Point");
+            _spline.AddCurve();
+            EditorUtility.SetDirty(_spline);
+        }
+    }
+
+
+    private void OnSceneGUI()
+    {
+        _spline = target as CameraPath;
+        _handleTransform = _spline.transform;
+        _handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            _handleTransform.rotation : Quaternion.identity;
+
+        Vector3 p0 = ShowPoint(0);
+        for (int i = 1; i < _spline.ControlPointCount; i += 3)
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i+1);
+            Vector3 p3 = ShowPoint(i+2);
+
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+            p0 = p3;
+        }
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = _spline.GetPoint(index);
+        float size = HandleUtility.GetHandleSize(point);
+        Handles.color = _spline.IsPathPoint(index) ? Color.cyan : Color.gray;
+        if (Handles.Button(point, _handleRotation, size * HandleSize, size * PickSize, Handles.DotCap))
+        {
+            _selectedIndex = index;
+            _spline.Time = _spline.GetTime(index);
+            Repaint();
+        }
+        if (_selectedIndex == index)
+        {
+            if (Tools.current == Tool.Move)
+            {
+                EditorGUI.BeginChangeCheck();
+                point = Handles.DoPositionHandle(point, _handleRotation);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObjects(new Object[] { _spline.transform, _spline }, "Move Camera");
+                    EditorUtility.SetDirty(_spline);
+                    _spline.SetPoint(index, point);
+                    _spline.Time = _spline.GetTime(index);
+                }
+            }
+            else if (Tools.current == Tool.Rotate && _spline.IsPathPoint(_selectedIndex))
+            {
+                var rotation = _spline.GetRotation(index);
+                EditorGUI.BeginChangeCheck();
+                rotation = Handles.DoRotationHandle(rotation, point);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObjects(new Object[]{_spline.transform,_spline}, "Rotate Camera");
+
+                    EditorUtility.SetDirty(_spline);
+                    _spline.SetRotation(index, rotation);
+                    _spline.Time = _spline.GetTime(index);
+                }
+            }
+        }
+        return point;
+    }
+}
---
+++ Assets/Scripts/CameraPath.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraPath : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] _points;
+
+    [SerializeField] 
+    private Quaternion[] _rotations;
+
+    [SerializeField]
+    private float _time;
+    public float Time
+    {
+        get { return _time; }
+        set
+        {
+            _time = Mathf.Clamp01(value);
+            SetCameraPosition();
+            SetCameraRotation();
+        }
+    }
+
+    public int ControlPointCount
+    {
+        get
+        {
+            return _points.Length;
+        }
+    }
+
+    void Update()
+    {
+        SetCameraPosition();
+        SetCameraRotation();
+    }
+
+    public void SetCameraPosition()
+    {
+        if (enabled)
+        {
+            transform.position = GetPoint(_time);
+        }
+    }
+
+    public void SetCameraRotation()
+    {
+        if (enabled)
+        {
+            transform.rotation = GetRotation(_time);
+        }
+    }
+
+    public Vector3 GetPoint(int index)
+    {
+        return _points[index];
+    }
+
+
+    public void SetPoint(int index, Vector3 point)
+    {
+        if (IsPathPoint(index))
+        {
+            Vector3 delta = point - _points[index];
+            if (index > 0)
+            {
+                _points[index - 1] += delta;
+            }
+            if (index + 1 < _points.Length)
+            {
+                _points[index + 1] += delta;
+            }
+        }
+        _points[index] = point;
+
+        EnforceMode(index);
+    }
+    public Quaternion GetRotation(int index)
+    {
+        return _rotations[GetPathPoint(index)/3];
+    }
+
+    public void SetRotation(int index, Quaternion rotation)
+    {
+        if (IsPathPoint(index))
+        {
+            _rotations[index/3] = rotation;
+        }
+    }
+
+    public bool IsPathPoint(int index)
+    {
+        return index % 3 == 0;
+    }
+
+    public int GetSelectedPoint(float time)
+    {
+        var floatIndex = time * CurveCount * 3;
+        var intIndex = (int) floatIndex;
+        if (Math.Abs(floatIndex - intIndex) <= 0f)
+        {
+            return intIndex;
+        }
+        return -1;
+    }
+
+    public int GetPathPoint(int index)
+    {
+        if (index%3 <= 1)
+            return (index/3)*3;
+        return index + 1;
+    }
+
+    private void EnforceMode(int index)
+    {
+        int modeIndex = (index + 1) / 3;
+        if (modeIndex == 0 || modeIndex == (_points.Length - 1)/3)
+        {
+            return;
+        }
+
+        int middleIndex = modeIndex * 3;
+        int fixedIndex, enforcedIndex;
+        if (index <= middleIndex)
+        {
+            fixedIndex = middleIndex - 1;
+            enforcedIndex = middleIndex + 1;
+        }
+        else
+        {
+            fixedIndex = middleIndex + 1;
+            enforcedIndex = middleIndex - 1;
+        }
+
+        Vector3 middle = _points[middleIndex];
+        Vector3 enforcedTangent = middle - _points[fixedIndex];
+        _points[enforcedIndex] = middle + enforcedTangent;
+    }
+
+    void Reset()
+    {
+        var offset = new Vector3(1f, 0f, 0f);
+        _time = 0;
+
+        _points = new Vector3[] {
+			transform.position,
+			transform.position+offset,
+			transform.position+2f*offset,
+			transform.position+3*offset
+		};
+
+        _rotations = new Quaternion[]
+        {
+            transform.rotation,
+            transform.rotation
+        };
+    }
+
+    public int CurveCount
+    {
+        get
+        {
+            return (_points.Length - 1) / 3;
+        }
+    }
+
+    public void AddCurve()
+    {
+        Vector3 point = _points[_points.Length - 1];
+        Array.Resize(ref _points, _points.Length + 3);
+        point.x += 1f;
+        _points[_points.Length - 3] = point;
+        point.x += 1f;
+        _points[_points.Length - 2] = point;
+        point.x += 1f;
+        _points[_points.Length - 1] = point;
+
+        Array.Resize(ref _rotations, _rotations.Length + 1);
+        _rotations[_rotations.Length - 1] = _rotations[_rotations.Length - 2];
+
+        EnforceMode(_points.Length - 4);
+        _time = _time*(CurveCount - 1)/CurveCount;
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = _points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i =  Mathf.FloorToInt(t);
+            t -= i;
+            i *= 3;
+        }
+        return Bezier.GetPoint( _points[i], _points[i + 1], _points[i + 2], _points[i + 3], t);
+    }
+
+    public Quaternion GetRotation(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            return _rotations[_rotations.Length - 1];
+        }
+        t = Mathf.Clamp01(t) * CurveCount;
+        i = Mathf.FloorToInt(t);
+        t -= i;
+        return Quaternion.Lerp(_rotations[i],_rotations[i+1],t);
+    }
+
+    public float GetTime(int index)
+    {
+        return index/(float)(CurveCount*3);
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = _points.Length - 4;
+        }
+        else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return Bezier.GetFirstDerivative(
+            _points[i], _points[i + 1], _points[i + 2], _points[i + 3], t);
+    }
+
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
+}
